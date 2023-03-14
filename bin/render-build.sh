21aa364544c20d2a72978c05e9b6eee563bff6e0
abraham <apolichenco@gmail.com>
2023-03-14 10:31:24 -0400

    blaxsax

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,15 +9,17 @@ cp -a client/build/. public/
 
 # builds the back end code
 bundle install
+rails db:migrate:down VERSION=20230223195752
 # bundle exec rake db:migrate
-# bundle exec rake db:rollback --trace
+bundle exec rake db:rollback --trace
 # bundle exec rake db:rollback
 # bundle exec rake db:rollback
 # bundle exec rake db:rollback
 # bundle exec rake db:rollback
 # bundle exec rake db:rollback
 # bundle exec rake db:rollback
-bundle exec rake db:migrate --trace
+
+# bundle exec rake db:migrate --trace
 
 
 # bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
