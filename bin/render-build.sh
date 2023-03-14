bb5982a200165f9f0a5ee54bf95685b714f03628
abraham <apolichenco@gmail.com>
2023-03-14 10:56:17 -0400

    initial

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,25 +2,8 @@
 # exit on error
 set -o errexit
 
-# builds the front end code
-rm -rf public
-npm install --prefix client && npm run build --prefix client
-cp -a client/build/. public/
-
-# builds the back end code
 bundle install
-DROP TABLE users
-# rails db:migrate:down VERSION=20230223195752
-# bundle exec rake db:migrate
-# bundle exec rake db:rollback --trace
-# bundle exec rake db:rollback
-# bundle exec rake db:rollback
-# bundle exec rake db:rollback
-# bundle exec rake db:rollback
-# bundle exec rake db:rollback
-# bundle exec rake db:rollback
-
-# bundle exec rake db:migrate --trace
-
-
-# bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
+# bundle exec rake assets:precompile # These lines are commented out because we have an API only app
+# bundle exec rake assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seed
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-# first_user = User.create([{ name: "Abe Poli", password: "reach4Sky", percentage: 10 }])
+first_user = User.create([{ name: "Abe Poli", password: "reach4Sky", percentage: 10 }])
 income = Income.create([{ amount: 2000, user_id: 1}])
-# charity = Charity.create([{ name: "Chabad Carmel Valley", link: "https://www.chabadcv.com/templates/articlecco_cdo/aid/437313/jewish/Donate.htm"}])
-# donation = Donation.create([{ amount: 180, user_id: 1, charity_id: 1}])
-# favorite_charity = FavCharity.create([{ user_id: 1, charity_id: 1}])
-# subscription = Subscription.create([{ amount: 180, day_of_month: 25, user_id: 1, charity_id: 1}])
+charity = Charity.create([{ name: "Chabad Carmel Valley", link: "https://www.chabadcv.com/templates/articlecco_cdo/aid/437313/jewish/Donate.htm"}])
+donation = Donation.create([{ amount: 180, user_id: 1, charity_id: 1}])
+favorite_charity = FavCharity.create([{ user_id: 1, charity_id: 1}])
+subscription = Subscription.create([{ amount: 180, day_of_month: 25, user_id: 1, charity_id: 1}])
 
 
 
