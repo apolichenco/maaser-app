fdd9a8aef5e11c60df0ef66d34fe84d491d42236
abraham <apolichenco@gmail.com>
2023-03-14 10:20:14 -0400

    login and signup created

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,5 +9,13 @@ cp -a client/build/. public/
 
 # builds the back end code
 bundle install
-bundle exec rake db:migrate
+# bundle exec rake db:migrate
+bundle exec rake db:rollback
+bundle exec rake db:rollback
+bundle exec rake db:rollback
+bundle exec rake db:rollback
+bundle exec rake db:rollback
+bundle exec rake db:rollback
+bundle exec rake db:rollback
+
 # bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
